551c5693cdee2f88d9f8cf7dee7abda5bcf666ef
Todd LaMothe <todd@Todds-Macbook-Pro.local>
2021-05-20 14:54:57 -0400

    Added backlog fetch

--- eng-metrics-api/functions/backlog-handlers.js
+++ eng-metrics-api/functions/backlog-handlers.js
@@ -1,9 +1,22 @@
 module.exports.backlogs = async (event) => {
-    console.log("[backlogs]");
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+
+    fetch('https://unionstmedia.atlassian.net/rest/agile/1.0/board/')
+        .then(response => {
+            if (!response.ok) {
+                const response = {
+                    statusCode: response.status,
+                    body: JSON.stringify('Error fetching product backlogs')
+                };        
+                return response;
+                    throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json()            
+        })
+        .then(data => {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify('Hello from Lambda!')
+            };        
+            return response;
+        });
 };
