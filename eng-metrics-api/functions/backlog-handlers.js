0b0575e76f7c980ac7f1abf1a4f19f858caaceb6
Todd LaMothe <todd@Todds-Macbook-Pro.local>
2021-05-21 16:04:26 -0400

    Working (untested) API call to fetch boards

--- eng-metrics-api/functions/backlog-handlers.js
+++ eng-metrics-api/functions/backlog-handlers.js
@@ -1,22 +1,44 @@
-module.exports.backlogs = async (event) => {
+const fetch = require('node-fetch');
 
-    fetch('https://unionstmedia.atlassian.net/rest/agile/1.0/board/')
+module.exports.backlogs = async (event, context, callback) => {
+    await fetch(
+        'https://unionstmedia.atlassian.net/rest/agile/1.0/board/', {
+        method: 'GET',
+        headers: {
+            Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
+        }
+     })
         .then(response => {
             if (!response.ok) {
                 const response = {
                     statusCode: response.status,
-                    body: JSON.stringify('Error fetching product backlogs')
-                };        
-                return response;
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    body: response.statusText
+                };
+                callback(JSON.stringify(responseMessage));
             }
             return response.json()            
         })
         .then(data => {
-            const response = {
+            responseBody = {
+                'boardCount': data.total,
+                'boards': data.values
+            }
+        const responseMessage = {
                 statusCode: 200,
-                body: JSON.stringify('Hello from Lambda!')
-            };        
-            return response;
+                body: responseBody
+            }; 
+            callback(null, JSON.stringify(responseMessage));
+        }).catch((error) => {
+            callback(Error(error));
         });
 };
+
+module.exports.backlogEpics = async (event, context, callback) => {
+    response = {
+        'status' : 200,
+        'body': 'EPIC!'
+    }
+    callback(null, JSON.stringify(response));
+}
+
+// module.exports.backlogs(null, null, (error, response) => console.log(JSON.parse(response).body))
