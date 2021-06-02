082abf7ca76a32267f6296c14e00d125e25a9c37
Todd LaMothe <todd@Todds-Macbook-Pro.local>
2021-06-02 17:03:25 -0400

    Added epic-issues endpoine and a monster epic function that needs help

--- eng-metrics-api/functions/backlog-handlers.js
+++ eng-metrics-api/functions/backlog-handlers.js
@@ -37,89 +37,6 @@ module.exports.backlogs = async (event, context, callback) => {
         });
 };
 
-// module.exports.backlogEpicsv1 = async (event, context, callback) => {
-//     if (!event.pathParameters.backlogId) {
-//         const responseMessage = {
-//             statusCode: 500,
-//             body: "Backlog ID not provided"
-//         };
-//         callback(JSON.stringify(responseMessage));
-//     }
-    
-//     backlogId = event.pathParameters.backlogId;
-//     backlogEpicsUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/board/" + backlogId + "/epic";
-//     var responseBody
-//     var epicArray;
-
-//     console.log("1");
-
-//     // Fetch epics
-//     await fetch(
-//         backlogEpicsUri, {
-//         method: 'GET',
-//         headers: {
-//             Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
-//         }
-//      })
-//      .then(response => {
-//         return response.json()            
-//     })
-//     .then(data => {
-//         console.log("2");
-//         epicArray = data.values.map((element, index, array) => {
-//             return { 
-//                 "id" : element.id,
-//                 "key" : element.key,
-//                 "name" : element.name
-//             }
-//         })
-//     }).catch((error) => {
-//         callback(Error(error));
-//     });
-
-//     // For each epic, fetch epic issues and tally counts and point totals
-//     var epicIssueTotal, epicIssuesToDo, epicIssuesInProgress, epicIssuesDone, epicPointTotal;
-//     console.log("3");
-//     var epicArrayWithIssues = epicArray.map( (element, index, array) => {
-//         var epicIssuesUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/epic/" + element.key + "/issue"
-//         // Fetch EPIC issues
-//         fetch(
-//             epicIssuesUri, {
-//             method: 'GET',
-//             headers: {
-//                 Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
-//             }
-//             })
-//             .then(response => {
-//             return response.json()            
-//         })
-//             .then(data => {
-//                 console.log("4");
-//                 // console.log(data);
-//             })
-//             .catch((error) => {
-//             callback(Error(error));
-//         });        
-//     })
-//     console.log("5");
-
-//     storyPointEstimateField = "customfield_10035";
-
-//     responseBody = {
-//         epicCount: epicArray.length,
-//         epics: epicArray
-//     }
-
-//     const responseMessage = {
-//         "isBase64Encoded": false,
-//         "statusCode": 200,
-//         "headers": {
-//         },
-//         "body": JSON.stringify(responseBody)
-//     }            
-//     callback(null, responseMessage);
-// }
-
 // Get all epics for the backlog containing the "Budget-Reporting" tag
 // Then talle the total points, stories done/in progress/to do
 module.exports.backlogEpics = async (event, context, callback) => {
@@ -131,11 +48,13 @@ module.exports.backlogEpics = async (event, context, callback) => {
         callback(JSON.stringify(responseMessage));
     }
 
-    // 1. Fetch the backlog
-    backlogId = event.pathParameters.backlogId;
-    backlogEpicsUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/board/" + backlogId + "/epic";
+    const budgetReportingTag = "Budget-Reporting";
     var responseBody
-    var epicArray;
+    var epicArray, budgetReportingEpics = [];
+    backlogId = event.pathParameters.backlogId;
+
+    // 1. Fetch the backlog
+    backlogEpicsUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/board/" + backlogId + "/epic";
 
     console.log("1");
 
@@ -154,7 +73,7 @@ module.exports.backlogEpics = async (event, context, callback) => {
 
         console.log("2");
 
-        epicArray = data.values.map((element, index, array) => {
+        epicArray = data.values.map((element, index, array) => {            
             return { 
                 "id" : element.id,
                 "key" : element.key,
@@ -167,6 +86,34 @@ module.exports.backlogEpics = async (event, context, callback) => {
 
     console.log("3");
 
+    // Only include the epic if it was labeled with the budget reporting tag
+    for (const epic of epicArray) {
+        epicLabelsUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/issue/" + epic.key + "?fields=labels"
+        await fetch(
+            epicLabelsUri, {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
+                }
+             })     .then(response => {
+                return response.json()            
+            })
+            .then(data => {
+        
+                console.log("4");
+                if (data.fields.labels && data.fields.labels[0] && data.fields.labels[0] == budgetReportingTag) {
+                    // Include this budget reporting epic
+                    budgetReportingEpics.push(epic);
+                }
+        
+                
+            }).catch((error) => {
+                callback(Error(error));
+            });
+        
+    }
+
+
     // 2. epicArray now contains an array of backlog epics.
     //    For each epic labeled Budget-Reporting, fetch all issues
 
@@ -176,45 +123,19 @@ module.exports.backlogEpics = async (event, context, callback) => {
     // But, need to figure out how to nest a synchronous fetch inside map reduce
 
     var epicIssuesUri;
-    epicArray.forEach( async function (epic) {
+    for (const epic of budgetReportingEpics) {
+        // Only include if the epic was tagged with the budget reporting tag
+
         console.log(epic.name)
-        epicIssuesUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/epic/" + epic.key + "/issue";
-        await fetch(
-            epicIssuesUri, {
-            method: 'GET',
-            headers: {
-                Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
-            }
-         })
-         .then(response => {
-            return response.json()            
-        })
-    
+        console.log("5");
+        await module.exports.epicIssues({pathParameters: { backlogId: 23, epicId : epic.id}}, null, (error, response) => {
+            console.log("6");
+            // 3. Tally up stats for this epic
 
-    })
-
-    // await Promise.all(epicArray.map(epic => fetchEpicIssues(epic)))
-    //     .then(function (responses) {
-    //         // Get a JSON object from each of the responses
-    //         return Promise.all(responses.map(function (response) {
-    //             return response.json();
-    //         }));
-    //     }).then(function (data) {
-    //         // Log the data to the console
-    //         // You would do something with both sets of data here
-    //         console.log(data);
-    //     }).catch(function (error) {
-    //         // if there's an error, log it
-    //         console.log(error);
-    //     });
-
-        
-
-    console.log("5");
-
-
-
-    //      3. Tally up stats for this epic
+        });
+        console.log("7");
+    }
+    console.log("8");
 
     // 4. Return retults
     var responseBody = {
@@ -232,25 +153,51 @@ module.exports.backlogEpics = async (event, context, callback) => {
 
 }
 
-function fetchEpicIssues(epic) {
-    console.log("epic = ", epic);
-    epicIssuesUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/epic/" + epic.key + "/issue";
-    fetchPromise = fetch(
+module.exports.epicIssues = async (event, context, callback) => {
+    if (!event.pathParameters.backlogId || !event.pathParameters.epicId) {
+        const responseMessage = {
+            statusCode: 500,
+            body: "Backlog ID and epic ID required"
+        };
+        callback(JSON.stringify(responseMessage));
+    }
+
+    var epicIssuesUri = "https://unionstmedia.atlassian.net/rest/agile/1.0/epic/" + event.pathParameters.epicId + "/issue";
+    await fetch(
         epicIssuesUri, {
         method: 'GET',
         headers: {
             Authorization: 'Basic dGxhbW90aGVAdW5pb25zdG1lZGlhLmNvbTp5R2xrV3RVZ2V5d1Jjbk9SZ25uRkQ1Nzg=',
         }
      })
-    //  .then( response => {
-    //      return response.json()
-    //  })
-    //  .then( data => {
-    //      console.log(data)
-    //  })
+        .then(response => {
+            if (!response.ok) {
+                const responseMessage = {
+                    statusCode: response.status,
+                    body: response.statusText
+                };
+                callback(JSON.stringify(responseMessage));
+            }
+            return response.json()            
+        })
+        .then(data => {
+            const responseBody = {
+                issues: data.issues
+            }
 
-     return fetchPromise;
-}
+            const responseMessage = {
+                "isBase64Encoded": false,
+                "statusCode": 200,
+                "headers": {
+                },
+                "body": JSON.stringify(responseBody)
+            }            
+            callback(null, responseMessage);
+        }).catch((error) => {
+            callback(Error(error));
+        });
+};
 
 
+// module.exports.epicIssues({pathParameters: { backlogId: 23, epicId : "A20-2137"}}, null, (error, response) => console.log(response))
 module.exports.backlogEpics({pathParameters: { backlogId: 23}}, null, (error, response) => console.log(response))
