43ea3d3852331b0a11a9cb0f71ea0c4acbf7da05
Todd LaMothe <todd@Todds-Macbook-Pro.local>
2021-06-09 13:03:37 -0400

    Added serverless variable for API gateway base url

--- eng-metrics-api/functions/google-doc-handlers.js
+++ eng-metrics-api/functions/google-doc-handlers.js
@@ -2,7 +2,8 @@ const fetch = require('node-fetch');
 
 module.exports.exportBacklogEpicsGoogleSheet = async (event, context, callback) => {
     console.log("[exportBacklogEpicsGoogleSheet]");
+    console.log("process.env.GW_URL = ", process.env.GW_URL);
     callback(null);
 }
 
-module.exports.exportBacklogEpicsGoogleSheet({}, null, () => console.log("callback ;)"));
+// module.exports.exportBacklogEpicsGoogleSheet({}, null, () => console.log("callback ;)"));
