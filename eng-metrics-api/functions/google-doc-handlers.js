c5ae73b3611f47038e30826d97b384fb2c12142a
Todd LaMothe <todd@Todds-Macbook-Pro.local>
2021-06-09 22:47:16 -0400

    About to test writing to Google sheet

--- eng-metrics-api/functions/google-doc-handlers.js
+++ eng-metrics-api/functions/google-doc-handlers.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-module.exports.exportBacklogEpicsGoogleSheet = (event, context, callback) => {
+module.exports._exportBacklogEpicsGoogleSheet = (event, context, callback) => {
     if (!event.backlogId) {
         const responseMessage = {
             statusCode: 500,
@@ -46,9 +46,39 @@ module.exports.exportBacklogEpicsGoogleSheet = (event, context, callback) => {
             // callback(null, responseMessage);
         }).catch((error) => {
             callback(Error(error));
-        });
-
-    
+        });   
 }
+
+module.exports.exportBacklogEpicsGoogleSheet = async event => {
+    console.log('Starting write function');
+  
+    if(!event.body) {
+      return formatResponse(400, { message: 'body is missing' });
+    }
+    const body = JSON.parse(event.body);
+  
+    if(!body.cells || !Array.isArray(body.cells)) {
+      return formatResponse(400, { message: '"cells" should be an array' })
+    }
+  
+    // load up everything that's necessary to work with cells
+    await spreadsheetAuth(doc);
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    await sheet.loadCells();
+    for(const { identifier, content } of body.cells) {
+      const cell = sheet.getCellByA1(identifier);
+      cell.value = content;
+    }
+    await sheet.saveUpdatedCells();
+    return formatResponse(200, { message: 'Cells saved successfully'});
+  };
+
+function spreadsheetAuth(document) {
+    return document.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    });
+  }
 
 // module.exports.exportBacklogEpicsGoogleSheet({}, null, () => console.log("callback ;)"));
