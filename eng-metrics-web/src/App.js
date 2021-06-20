8a8ee13c4d55defd8ce525ca032f330d432b2b42
Todd LaMothe <todd@Todds-MacBook-Air.local>
2021-06-20 09:34:11 -0400

    Can fetch epic data from API

--- eng-metrics-web/src/App.js
+++ eng-metrics-web/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { PieChart } from 'react-dc-js';
+import crossfilter from 'crossfilter2';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [cx, setCx] = useState(null);
+
+  (async () => {
+    console.log("[in async]");
+
+    let baseUrl = "https://ausl4ri6y1.execute-api.us-east-1.amazonaws.com/test-tl/backlogs/32/epics";
+    let response = await fetch(baseUrl, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+    let data = await response.json();
+    console.log("got data:");
+    console.log(data);
+
+  //   const data = await d3.csv(csv)
+  //   const dateFormatSpecifier = '%m/%d/%Y'
+  //   const dateFormatParser = d3.timeParse(dateFormatSpecifier)
+  //   data.forEach(d => {
+  //     d.dd = dateFormatParser(d.date)
+  //     d.month = d3.timeMonth(d.dd) // pre-calculate month for better performance
+  //     d.close = +d.close // coerce to number
+  //     d.open = +d.open
+  //   })
+  //   const cx = crossfilter(data)
+  //   setCx(cx)
+  })()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Here is some data.
     </div>
   );
 }
