c4f5ccc82499577d56a77345e0b39f97e71d8d6d
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-09-17 16:28:20 -0400

    Cleanup

--- eng-metrics-api/functions/backlog-handlers.js
+++ eng-metrics-api/functions/backlog-handlers.js
@@ -170,4 +170,4 @@ function formatEpicObject(epicObject) {
 
 // module.exports.backlogEpics({pathParameters: { backlogId: 32}}, {}, (error, response) => console.log(response))
 // module.exports.backlogSprints({pathParameters: { backlogId: 23}}, {}, (error, response) => console.log(response))
-module.exports.backlogVelocity({pathParameters: { backlogId: 23}}, {}, (error, response) => console.log(response))
+// module.exports.backlogVelocity({pathParameters: { backlogId: 23}}, {}, (error, response) => console.log(response))
