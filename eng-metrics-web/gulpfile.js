1a8c2e9179cead3767652b4ceb22807283859726
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-10-30 14:14:56 -0400

    Commented out gulpfile

--- eng-metrics-web/gulpfile.js
+++ eng-metrics-web/gulpfile.js
@@ -1,7 +1,7 @@
-const gulp = require("gulp");
-const gap = require("gulp-append-prepend");
+// const gulp = require("gulp");
+// const gap = require("gulp-append-prepend");
 
-async function license() {
+// async function license() {
 //   // this is to add Creative Tim licenses in the production mode for the minified js
 //   gulp
 //     .src("build/static/js/*chunk.js", { base: "./" })
@@ -22,7 +22,7 @@ async function license() {
 //     )
 //     .pipe(gulp.dest("./", { overwrite: true }));
 //   return;
-}
+// }
 
-gulp.task("licenses", license);
-gulp.task("default", license);
+// gulp.task("licenses", license);
+// gulp.task("default", license);
