88e5886e60e60ae42435e0886b9814210e59f1c2
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-10-30 13:39:36 -0400

    Fixed deployment bugs and updated package.json

--- eng-metrics-web/gulpfile.js
+++ eng-metrics-web/gulpfile.js
@@ -7,45 +7,7 @@ gulp.task("licenses", async function () {
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(
       gap.prependText(`/*!
-
-=========================================================
-* Argon Dashboard Material-UI - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-material-ui/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
-
-  // this is to add Creative Tim licenses in the production mode for the minified html
-  gulp
-    .src("build/index.html", { base: "./" })
-    .pipe(
-      gap.prependText(`<!--
-
-=========================================================
-* Argon Dashboard Material-UI - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-material-ui/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+Union Street Media - Engineering Metrics DashboardUnion Street Media - Engineering Metrics Dashboard
 -->`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
@@ -55,21 +17,7 @@ gulp.task("licenses", async function () {
     .src("build/static/css/*chunk.css", { base: "./" })
     .pipe(
       gap.prependText(`/*!
-
-=========================================================
-* Argon Dashboard Material-UI - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-material-ui/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+      Union Street Media - Engineering Metrics Dashboard
 */`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
