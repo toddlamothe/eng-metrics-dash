10940f063236f91c9a64c3f1b636ef6f86a70938
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-10-30 14:10:22 -0400

    Removed gulp licenses tasks - breaking the build

--- eng-metrics-web/gulpfile.js
+++ eng-metrics-web/gulpfile.js
@@ -2,26 +2,26 @@ const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
 async function license() {
-  // this is to add Creative Tim licenses in the production mode for the minified js
-  gulp
-    .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-Union Street Media - Engineering Metrics DashboardUnion Street Media - Engineering Metrics Dashboard
--->`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
+//   // this is to add Creative Tim licenses in the production mode for the minified js
+//   gulp
+//     .src("build/static/js/*chunk.js", { base: "./" })
+//     .pipe(
+//       gap.prependText(`/*!
+// Union Street Media - Engineering Metrics DashboardUnion Street Media - Engineering Metrics Dashboard
+// -->`)
+//     )
+//     .pipe(gulp.dest("./", { overwrite: true }));
 
-  // this is to add Creative Tim licenses in the production mode for the minified css
-  gulp
-    .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-Union Street Media - Engineering Metrics Dashboard
-*/`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
-  return;
+//   // this is to add Creative Tim licenses in the production mode for the minified css
+//   gulp
+//     .src("build/static/css/*chunk.css", { base: "./" })
+//     .pipe(
+//       gap.prependText(`/*!
+// Union Street Media - Engineering Metrics Dashboard
+// */`)
+//     )
+//     .pipe(gulp.dest("./", { overwrite: true }));
+//   return;
 }
 
 gulp.task("licenses", license);
