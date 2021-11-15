21ae1bbff9d829d43bf38b6b2f8277a051a59be4
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-11-15 16:15:53 -0500

    Moved pie chart to its own row and shrank

--- eng-metrics-web/src/components/Charts/Pie.js
+++ eng-metrics-web/src/components/Charts/Pie.js
@@ -4,10 +4,14 @@ import { Pie } from 'react-chartjs-2';
 const PieChart = (props) => {
     return (
         <>
-            <Pie height={250} data={props.data} options={{
-            legend:{display:true},
-            title: {display: false},
-        }} />
+            <Pie height={150} data={props.data} options={{
+                legend:{display:false},
+                title: {display: false},
+                tooltips: {
+                    enabled: true
+                },          
+            }} 
+        />
         </>
         );      
 }
